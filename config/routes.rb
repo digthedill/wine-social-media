223d0f7f03ee0ccc0faab32c5410ca9553cb9cca
digthedill <popssly@gmail.com>
2021-02-03 17:35:55 -0600

    added devise and set custom views

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
 
+  devise_for :users
   root to: "main#index"
+
+#   unauthenticated do
+#     root :to => 'main#index'
+#  end
+ 
+#  authenticated do
+#    root :to => 'dashboard#index'
+#  end
 
 
 end
